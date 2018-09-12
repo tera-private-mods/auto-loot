959d2c096ae49ccc1298c206ed4c66efe3ce88c4
valkyr1e <nkpoidjp+tera@gmail.com>
2018-09-13 02:38:03 +0900

    fix setting migrator

--- settings_migrator.js
+++ settings_migrator.js
@@ -13,17 +13,17 @@ const DefaultSettings = {
 module.exports = function MigrateSettings(from_ver, to_ver, settings) {
     if (from_ver === undefined) {
         // Migrate legacy config file
-        return DefaultSettings;
+        return Object.assign(Object.assign({}, DefaultSettings), settings);
     } else if (from_ver === null) {
         // No config file exists, use default settings
         return DefaultSettings;
     } else {
-        if(from + 1 < to) {
+        if (from_ver + 1 < to_ver) {
             settings = MigrateSettings(from_ver, from_ver + 1, settings);
             return MigrateSettings(from_ver + 1, to_ver, settings);
         }
 
-        switch (to) {
+        switch (to_ver) {
             default:
                 settings = Object.assign(DefaultSettings, settings);
                 break;
