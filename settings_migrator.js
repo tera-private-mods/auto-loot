7797dde469c1edd7943f1ef0377da7287c75f1d3
valkyr1e <nkpoidjp+tera@gmail.com>
2018-09-13 02:59:07 +0900

    fix migrate script

--- settings_migrator.js
+++ settings_migrator.js
@@ -18,12 +18,17 @@ module.exports = function MigrateSettings(from_ver, to_ver, settings) {
         // No config file exists, use default settings
         return DefaultSettings;
     } else {
+        // Migrate from older version (using the new system) to latest one
         if (from_ver + 1 < to_ver) {
+            // Recursively upgrade in one-version steps
             settings = MigrateSettings(from_ver, from_ver + 1, settings);
             return MigrateSettings(from_ver + 1, to_ver, settings);
         }
 
         switch (to_ver) {
+            case 2:
+                settings.blacklist.push(98599, 98600);
+                break;
             default:
                 settings = Object.assign(DefaultSettings, settings);
                 break;
